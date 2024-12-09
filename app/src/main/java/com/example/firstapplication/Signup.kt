9c10b18da662383f836fd3fe3af8ddef79f75181
kamaubryan <briankamau344@gmail.com>
2024-12-09 10:14:36 +0300

    added some functionalities to the submit button

--- app/src/main/java/com/example/firstapplication/Signup.kt
+++ app/src/main/java/com/example/firstapplication/Signup.kt
@@ -1,6 +1,7 @@
 package com.example.firstapplication
 
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
@@ -11,10 +12,20 @@ class Signup : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.signuppage)
-        val username: EditText = findViewById(R.id.editTextText)
-        val email: EditText = findViewById(R.id.editTextTextEmailAddress)
-        val password: EditText = findViewById(R.id.editTextTextPassword2)
+        val email: EditText = findViewById(R.id.editTextTextEmailAddress2)
+        val password: EditText = findViewById(R.id.editTextTextPassword)
         val signupButton: Button = findViewById(R.id.button3)
+
+        signupButton.setOnClickListener{
+            val emailText = email.text.toString().trim()
+            val passwordText = password.text.toString().trim()
+
+
+             if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
+                email.error = "Invalid email address"
+            }else if (passwordText.isBlank()) {
+                password.error = "Password is required"
+            }}
 
     }
 }
