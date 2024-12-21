d0651da7858d5d06533eb9bec63a6d45a2ce484d
kamaubryan <briankamau344@gmail.com>
2024-12-21 22:16:26 +0300

    added some goggle version and confgured the version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,22 +26,31 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
+    dependencies {
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.androidx.activity)
+        implementation(libs.androidx.constraintlayout)
+        implementation(libs.material)  // This will automatically resolve to 'com.google.android.material:material:1.9.0'
+    }
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+
+    // Material 3
+//    implementation(com.google.android.material:material:1.9.0)
+
+    // Test libraries
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
